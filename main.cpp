417638a5c4b0a48074b9d65a37d2546cd7e5be4c
Leon <doncorleone142857@gmail.com>
2022-01-17 18:53:24 +0700

    uncommenting and cleanup

--- main.cpp
+++ main.cpp
@@ -87,58 +87,14 @@ int main() {
 	Trie* root = new Trie(), *root2 = new Trie(), *root3 = new Trie();
 	static const int STRING_LENGTH = 6, STRING_LENGTH2 = 20, STRING_LENGTH3 = 100,
 	SUGGEST_LENGTH = 3, SUGGEST_LENGTH2 = 6, SUGGEST_LENGTH3 = 10;
-//	time_count_insert(root, STRING_LENGTH);
-//	time_count_insert(root2, STRING_LENGTH2);
-//	time_count_insert(root3, STRING_LENGTH3);
-//	time_count_suggest_util(root, 10, STRING_LENGTH, 10, SUGGEST_LENGTH);
-//	time_count_suggest_util(root2, 100, STRING_LENGTH, 100, SUGGEST_LENGTH);
-//	time_count_suggest_util(root3, 1000, STRING_LENGTH, 1000, SUGGEST_LENGTH);
+	time_count_insert(root, STRING_LENGTH);
+	time_count_insert(root2, STRING_LENGTH2);
+	time_count_insert(root3, STRING_LENGTH3);
+
 	time_count_suggest_diffInput(root, STRING_LENGTH, 10, SUGGEST_LENGTH);
 	time_count_suggest_diffInput(root, STRING_LENGTH, 100, SUGGEST_LENGTH);
 	time_count_suggest_diffInput(root, STRING_LENGTH, 1000, SUGGEST_LENGTH);
 	time_count_suggest_diffInput(root, STRING_LENGTH, 10000, SUGGEST_LENGTH);
-
-//	cout << "insertion time for string of length " << STRING_LENGTH << endl;
-//	time_count_insert_util(root, 10, STRING_LENGTH);
-//	time_count_insert_util(root, 100, STRING_LENGTH);
-//	time_count_insert_util(root, 1000, STRING_LENGTH);
-//	time_count_insert_util(root, 10000, STRING_LENGTH);
-//	time_count_insert_util(root, 100000, STRING_LENGTH);
-
-
-//	chrono::steady_clock sc;   // create an object of `steady_clock` class
-//	vector<string> strings, strings2, strings3, strings4;
-//	for(int i = 0; i < 10; ++i)
-//		strings.push_back(random_string(STRING_LENGTH));
-//	for(int i = 0; i < 100; ++i)
-//		strings2.push_back(random_string(STRING_LENGTH));
-//	for(int i = 0; i < 1000; ++i)
-//		strings3.push_back(random_string(STRING_LENGTH));
-////	for(int i = 0; i < 10000; ++i)
-////		strings4.push_back(random_string(STRING_LENGTH));
-//	auto begin = sc.now();
-//	for(auto& str : strings){
-////		auto start = sc.now();     // start timer
-//		root->insert(root, str);
-////		auto end = sc.now();     // end timer
-////		auto time_span = static_cast<chrono::duration<double>>(end - start);
-////		cout << str << " " << time_span.count() << endl;
-//	}
-//	auto end = sc.now();
-//	auto time_loop = static_cast<chrono::duration<double>>(end - begin);
-//	cout << time_loop.count() << " seconds" << endl;
-//	begin = sc.now();
-//	for(auto& str: strings2)
-//		root->insert(root, str);
-//	end = sc.now();
-//	time_loop = static_cast<chrono::duration<double>>(end - begin);
-//	cout << time_loop.count() << " seconds" << endl;
-//	begin = sc.now();
-//	for(auto& str: strings3)
-//		root->insert(root, str);
-//	end = sc.now();
-//	time_loop = static_cast<chrono::duration<double>>(end - begin);
-//	cout << time_loop.count() << " seconds" << endl;
 
 //	root->insert(root, "hello");
 //	root->insert(root, "dog");
@@ -151,25 +107,25 @@ int main() {
 //	root->insert(root, "helping");
 
 //	int comp = root->printSuggestions(root, "hel");
-//	int option = 0;
-//	while(option != 3){
-//		cout << "Here's the options:\n1. Insert\n2. Suggest\n3. Quit" << endl;
-//		cin >> option;
-//		if(option == 1){
-//			cout << "What word do you want to type?" << endl;
-//			string word; cin >> word;
-//			root->insert(root, word);
-//		}
-//		else if(option == 2){
-//			cout << "Type a prefix to get suggestions:" << endl;
-//			string word; cin >> word;
-//			cout << "The suggestions are:\n";
-//			int comp = root->printSuggestions(root, word);
-//			if (comp == -1)
-//				cout << "No other strings found with this prefix\n";
-//			else if (comp == 0)
-//				cout << "No string found with this prefix\n";
-//		}
-//	}
+	int option = 0;
+	while(option != 3){
+		cout << "Here's the options:\n1. Insert\n2. Suggest\n3. Quit" << endl;
+		cin >> option;
+		if(option == 1){
+			cout << "What word do you want to type?" << endl;
+			string word; cin >> word;
+			root->insert(root, word);
+		}
+		else if(option == 2){
+			cout << "Type a prefix to get suggestions:" << endl;
+			string word; cin >> word;
+			cout << "The suggestions are:\n";
+			int comp = root->printSuggestions(root, word);
+			if (comp == -1)
+				cout << "No other strings found with this prefix\n";
+			else if (comp == 0)
+				cout << "No string found with this prefix\n";
+		}
+	}
 
 }
